bac7a07b86203e8593ef6235704cbd436333e3a6
OmarArturoGG <omararturomgg@gmail.com>
2026-02-04 22:03:52 -0600

    La guia 02

---
+++ src/main/java/sv/edu/udb/service/implementation/MovieServiceImpl.java
@@ -0,0 +1,27 @@
+package sv.edu.udb.service.implementation;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sv.edu.udb.repository.MovieRepository;
+import sv.edu.udb.repository.domain.Movie;
+import sv.edu.udb.service.MovieService;
+import java.util.Objects;
+
+//MG220764
+//Omar Arturo Maldonado Guzman
+
+@Getter
+@Service
+public class MovieServiceImpl implements MovieService {
+    private MovieRepository movieRepository;
+    @Autowired
+    public void setMovieRepository(final MovieRepository movieRepository) {
+        this.movieRepository = Objects.requireNonNull(movieRepository);
+    }
+    @Override
+    public Movie findMovieById(final Long id) {
+        return movieRepository.findMovieById(id);
+    }
+}
+

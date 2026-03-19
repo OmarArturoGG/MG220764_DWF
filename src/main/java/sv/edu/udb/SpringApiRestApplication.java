126e16f3a7674f27cb4766e86aafa1f4fd0321fc
OmarArturoGG <omararturomgg@gmail.com>
2026-03-18 23:28:08 -0600

    guia larga

---
+++ src/main/java/sv/edu/udb/SpringApiRestApplication.java
@@ -0,0 +1,17 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Omar Arturo Maldonado Guzman
+//MG220764
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
